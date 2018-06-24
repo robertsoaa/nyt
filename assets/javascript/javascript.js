cf0225c968129e2b5ca48dc46b093f3dde9824b2
Lamoliveira <LamOliveira@gmail.com>
2018-06-23 23:17:45 -0400

    v1.1 luiz update js

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,5 +1,5 @@
 // Globals
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=3c3850fc219544eca9e26a2398363927";
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=3c3850fc219544eca9e26a2398363927&type_of_material=news";
 var begindate = "";
 var enddate = "";
 var qttarticles = 10;
@@ -11,21 +11,29 @@ function showarticles(qttarticles, articles) { // function to show articles in t
     console.log(articles[0].snippet);
 
 
-    for (i = 0; i < qttarticles; i++) {
+    for (i = 0; i < qttarticles; i++) { // define fields to present %%%
         console.log("loop:" + i);
-        $("#articles").append("Article " + i + " " + articles[i].snippet + "<br>");
+        $("#articles").append("<h1>Article: " + i + "</h1><br>");
+        //$("#articles").append("   snippet: " +  articles[i].snippet + "<br>");
+        $("#articles").append("<h3>   headline: " + articles[i].headline.main + "</h3><br>");
+        $("#articles").append("  section: " + articles[i].section_name + "<br>");
+        $("#articles").append("  pub_date: " + articles[i].pub_date + "<br>");
+        if (articles[i].byline.original){ // find a way to test if exists. If the object does not have the property it causes an error. "Deferred exception: Cannot read property 'original' of undefined TypeError: Cannot read property 'original' of undefined"
+        $("#articles").append("  original: " + articles[i].byline.original + "<br>");
+        }
+        $("#articles").append("  web-url: " + articles[i].web_url + "<br>");
     }
 }
 
 function search(qttarticles, queryURL) {
-
+    console.log("url2:"+queryURL);
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
         // Step 1: Run this file, click a button, and see what the response object looks like in the browser's console.
         // Open up the data key, then open up the 0th, element. Study the keys and how the JSON is structured.
-
+        console.log(queryURL);
         console.log(response);
         var search = response.response.docs;
         console.log(search);
@@ -34,49 +42,33 @@ function search(qttarticles, queryURL) {
 }
 var goBtn = document.querySelector("#searchbutton");
 //
-goBtn.addEventListener("click", function() {
-//window.onload = function () {
-//    console.log("load");
-//}
-
-//$("btn btn-primary").on("click", function () {
-//    console.log("onclick btn");
-//    event.preventDefault();
-//    var input = $(this).attr("data-form-control");
-//});
-
-
-
-
-
-var term="";
-//$("#buttonSearch").on("click", function () {
-    $("#articles").append("zzzzzzzz");
-    console.log("Term1: " + term);
-    term = "&q=" + $("#searchTerm").val().trim();
-    queryURL = queryURL + term;
-    console.log("Term2: " + term);
+goBtn.addEventListener("click", function () {
+    queryURL = queryURL + "&q=" + $("#searchTerm").val().trim();
+    qttarticles = $("#retrieveRecords").val().trim();
 
     begindate = $("#startYear").val().trim() + "0101";
-    enddate = $("#endYear").val().trim() + "1201";
-
-    if (parseInt(begindate)) {
-        console.log(queryURL);
+    enddate = $("#endYear").val().trim() + "1231";
+    console.log("begindate:" + begindate);
+    console.log("enddate:" + enddate);
+    
+    
+    if (parseInt(begindate)>1900) {
         queryURL = queryURL + "&begin-date=" + begindate;
-        console.log(queryURL);
-    }
-    if (parseInt(enddate)) {
-        console.log(queryURL);
+        console.log("query"+queryURL);
+    } 
+
+    if (parseInt(enddate)>1900) {
         queryURL = queryURL + "&end-date=" + enddate;
-        console.log(queryURL);
+        console.log("query"+queryURL);
     }
+    console.log("qttarticles: " + qttarticles);
     if (parseInt(qttarticles)) {
         qttarticles = parseInt(qttarticles);
     }
     else {
         qttarticles = 10; //default 10
     }
-
+    console.log("url1:"+queryURL);
     search(qttarticles, queryURL);
 
 });
